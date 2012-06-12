3df6d412336d9a55fd23e20058b9ba4bcda1c9cf
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2012-06-12 18:56:43 -0300

    Inicio Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SearchFriend::Application.config.secret_token = '41ca5d5fda4c9071c03c82f1fc0b7dd88aa49350cdf25b74c432bb059e3f4bd2c7da8fc96a07c360e061277c396333abd02687c58c19b36a7f146bcc60fdbc04'
