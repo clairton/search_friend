beb9712e90a637c1a6326a000e520a1a3c2fc446
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2012-06-12 22:26:03 -0300

    internacionalização

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  #internacionalização
+  before_filter :localizate
+
+  #método responável pela internacionalização
+  def localizate
+    I18n.locale = params[:locale] || 'pt-BR'
+  end
 end
